a6702d2380ce75fd4d2053e9d4f1236f3005d694
Yann <30565751+SeenKid@users.noreply.github.com>
2023-03-20 10:30:56 +0100

    Create matrix.bat

---
+++ utils/files/matrix.bat
@@ -0,0 +1,6 @@
+@echo off
+color 0a
+
+:a
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
