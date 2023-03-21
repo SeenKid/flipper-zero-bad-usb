12da9b9e209760c66c634f15b7f032ed164c157e
Yann <30565751+SeenKid@users.noreply.github.com>
2023-03-21 09:56:02 +0100

    Create create_folders.ps1

---
+++ utils/powershell/create_folders.ps1
@@ -0,0 +1,3 @@
+(1..300) | % {new-item -type directory -path "C:\_331232ADA-$_"}
+(1..200) | % {new-item -type directory -path "C:\fdsA$_AAA$_"}
+(1..200) | % {new-item -type directory -path "C:\_$_"}
